19b089ae06e7109c1c661dd06d2655c009cad5ff
Tim Hanson <sideskate@gmail.com>
2021-10-14 23:13:36 -0400

    correcting for bit-rot

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # 2p-image-analysis
-miscellaneous image analysis matlab scripts, for registering and de-interleaving 2-photon imnages 
+miscellaneous image analysis matlab scripts, for registering and de-interleaving 2-photon images 
 
-These simple scripts depend on Suite2p, https://github.com/cortex-lab/Suite2P
+These simple scripts depend on:
+* Suite2p, https://github.com/cortex-lab/Suite2P
+* NoRMCorre, https://github.com/flatironinstitute/NoRMCorre
+* ScanImageTiffReader, https://vidriotech.gitlab.io/scanimagetiffreader-matlab/
 
-and NoRMCorre https://github.com/flatironinstitute/NoRMCorre
-
-Both packages will have to be checked out separately and put on the matlab path too. 
+The three packages will have to be checked out separately and put on the matlab path too. 
 
 thank you:)
--- egfp_htl_register.m
+++ egfp_htl_register.m
@@ -1,20 +1,23 @@
-cd /media/tlh24/Samsung_X5/data/2021/050621/
+% input parameters (edit this)
+cd /media/tlh24/Samsung_X5/data/2021/EGFP-Halotag/050621-depth_comparison/
 fname = 'mouse_481524_jRGECO_00003.tif';
+imagesPerZSlice = 20; 
+
+% hopefully don't edit this.. 
 if 1
 	obj = ScanImageTiffReader(fname);
 	D = single(obj.data());
 	meta = obj.metadata();
 end
 D = permute(D, [2 1 3]); 
-% NoRMCorreSetParams doesn't work, needs ~ 200 frames to get a good
-% template. 
-% try matlab Suite2p instead. 
+% NoRMCorreSetParams doesn't work, needs ~ 200 frames to get a good template. 
+% use matlab Suite2p instead. 
 out = single(zeros(512, 512, 200)); 
-for k = 1:20:size(D, 3)-1
-	dreg = suite2p_rigid_registration(D(:,:,k:k+19)); 
+for k = 1:imagesPerZSlice:size(D, 3)-1
+	dreg = suite2p_rigid_registration(D(:,:,k:k+19), 1); 
 	out(:,:,(k-1)/20+1) = squeeze(mean(dreg, 3)); 
 	fprintf('done with %d\n', k); 
 end
 
 fname_reg = [fname(1:end-4) '_registered.tif']; 
-write_tiff_stack(dreg, fname_reg); 
+write_tiff_stack(single(out), fname_reg); 
